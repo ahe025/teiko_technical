63a72e8130f53cfb6dbf61af080d8db3e8ed4054
amy <ahe025@berkeley.edu>
2026-05-20 18:57:36 -0700

    Python script SQLite

---
+++ load_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sqlite3
+
+df = pd.read_csv("cell-count.csv")
+
+conn = sqlite3.connect("cell_count.db")
+
+df.to_sql("cell_count", conn, if_exists = "replace", index = False)
+
+# CHECK TABLE
+# df_check = pd.read_sql_query("SELECT * FROM cell_count", conn)
+# print(df_check.head())
+
+# schema_info = pd.read_sql_query("PRAGMA table_info(cell_count)", conn)
+# print(schema_info)
+
+# print(df.shape)
+# print(df_check.shape)
+#
+
+conn.close()
+print("Database created successfully.")
