a2fcb0ce47584d4ade34fc682b94777d8378d756
amy <ahe025@berkeley.edu>
2026-05-22 17:51:09 -0700

    Supplemental question (Melanoma males)

---
+++ supp_question.py
@@ -0,0 +1,16 @@
+# Considering Melanoma males, what is the average number of B cells for responders at time=0? 
+
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect("cell_count.db")
+
+df = pd.read_sql_query("SELECT b_cell FROM cell_count " \
+                       "WHERE sex == 'M'" \
+                       "AND condition == 'melanoma'" \
+                       "AND response == 'yes'" \
+                       "AND time_from_treatment_start == 0", conn)
+
+print(df.mean())
+
+conn.close()
